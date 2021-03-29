5c2ee6d6f372a949eac67691fb1393632d706a1b
kumagai sou <s.kumagai.sys20@morijyobi.ac.jp>
2021-03-29 11:39:03 +0900

    給料計算アプリを追記
    kyuryo.htmlとsalary.htmlの新規作成
    app.pyとindex.htmlの更新

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def top():
     return render_template('index.html')
+
+@app.route('/kyuryo')
+def kyuryo():
+    return render_template('kyuryo.html')
+
+@app.route('/salary', methods=['POST'])
+def salary():
+    s = request.form.get('sal')
+    t = request.form.get('time')
+    n = int(s) * int(t)
+    return render_template('salary.html', n = n)
 
 if __name__ == '__main__':
     app.run(debug=True)
